32fa4c7b3302f0ae2a1ffb7ade74e768b96ac38f
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-02-04 11:03:04 -0800

    add click handler for switching page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { AgeCalculator } from './js/space';
+
+$(".start-btn").on("click", function () {
+  $(".page-1").fadeOut();
+  $(".page-2").fadeIn();
+});
