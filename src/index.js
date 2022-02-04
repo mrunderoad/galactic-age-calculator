9dc402472f69b5afca11d825edfef33c28697235
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-02-04 13:17:51 -0800

    age correctly displaying

--- src/index.js
+++ src/index.js
@@ -4,19 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { AgeCalculator } from './js/space';
 
-
+let calculator = new AgeCalculator();
 
 $("form#submit-planet").submit(function(event) {
   event.preventDefault();
   let years = parseInt($("#age").val());
   let lifeExpectancy = parseInt($("#expectancy").val());
   let planet = $("input:radio[name=planet]:checked").val();
-  let calculator = new AgeCalculator(years, lifeExpectancy);
-  let mars = calculator.marsAge();
+  let mars = calculator.marsAge(years);
   if(planet === "Mars") {
-    console.log(planet);
-    console.log(years);
-    console.log(calculator.marsAge());
+    $(".chosen-planet").html(planet);
     $(".planet-span").html(mars);
   } else
   console.log(lifeExpectancy);
