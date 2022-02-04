e1b97878fb5704b46ff6a7caae79dc1d505b73ca
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-02-04 11:54:34 -0800

    updating UIdisplay

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { AgeCalculator } from './js/space';
 
+
+
+$("form#submit-planet").submit(function(event) {
+  event.preventDefault();
+  let years = parseInt($("#age").val());
+  let lifeExpectancy = parseInt($("#expectancy").val());
+  let planet = $("input:radio[name=planet]:checked").val();
+  let calculator = new AgeCalculator(years, lifeExpectancy);
+  if(planet === "Mars") {
+    console.log();
+    $(".planet-span").html(calculator.marsAge);
+  }
+
+});
+
 $(".start-btn").on("click", function () {
-  $(".page-1").fadeOut();
-  $(".page-2").fadeIn();
+  $(".page-1").hide();
+  $(".page-2").show();
 });
--- src/js/space.js
+++ src/js/space.js
@@ -2,43 +2,43 @@ export class AgeCalculator {
   constructor(years, lifeExpectancy) {
   this.years = years;
   this.lifeExpectancy = lifeExpectancy;    
-  };
+  }
 
   mercuryAge(years) {
     return years / .24;
-  };
+  }
 
   mercuryLeft() {
     return this.lifeExpectancy / .24 - this.years / .24;
-  };
+  }
 
   venusAge(years) {
     return years / .62;
-  };
+  }
 
   venusLeft() {
     return this.lifeExpectancy / .62 - this.years / .62;
-  };
+  }
 
   marsAge(years) {
     return years / 1.88;
-  };
+  }
 
   marsLeft() {
     return this.lifeExpectancy / 1.88 - this.years / 1.88;
-  };
+  }
 
   jupiterAge(years) {
     return years / .0084;
-  };
+  }
 
   jupiterLeft() {
     return this.lifeExpectancy / .0084 - this.years / .0084;
-  };
+  }
 
   earthLeft() {
     return this.lifeExpectancy - this.years;
-  };
+  }
 
   overExpected() {
     return this.years - this.lifeExpectancy;
